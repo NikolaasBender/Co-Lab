d02362f52110af02242250db143f26542d02678a
Iva Majeticova <ivma2396@colorado.edu>
2019-04-20 13:56:52 -0600

    update posts and tasks

--- go_dev/tasks.go
+++ go_dev/tasks.go
@@ -3,7 +3,7 @@ package go_dev
 import (
   "database/sql"
   _ "github.com/lib/pq"
-  "fmt"
+  //"fmt"
 )
 
 func createTask(project_name,project_owner, task_name string,db *sql.DB)(bool) {
@@ -71,13 +71,48 @@ func updateStatus(project_name,project_owner, task_name,status int,db *sql.DB)(b
   	} else if (err != nil) {
     	return false
   	}
-
-	sqlStatement:= `UPDATE tasks
-  	SET status = $1
-  	WHERE project = $2 AND name = $3;`
-
-  	_, err = db.Exec(sqlStatement,status,parentID,task_name)
-
+	sqlStatement2:= `SELECT id,status FROM tasks WHERE project = $1 AND name = $2;`
+	
+	var oldStatus int
+	var taskID int
+	err = db.QueryRow(sqlStatement2,parentID,task_name).Scan(&taskID,&oldStatus)
+	if(err == sql.ErrNoRows) {
+   		return false
+  	} else if (err != nil) {
+    	return false
+  	}
+	var oldColumn string
+	if(oldStatus == 0){
+		oldColumn = "inprogress_tasks"
+	}
+	else if(oldStatus == 1){
+		oldColumn = "todo_tasks"
+	}
+	else {oldColumn = "completed_tasks"}
+	
+	var newColumn string
+	if(status == 0){
+		newColumn = "inprogress_tasks"
+	}
+	else if(status == 1){
+		newColumn = "todo_tasks"
+	}
+	else {newColumn = "completed_tasks"}
+	
+	sqlStatement3:= `UPDATE tasks SET status = $1 WHERE project = $2 AND name = $3;`
+  	_, err = db.Exec(sqlStatement3,status,parentID,task_name)
+	if(err != nil) {
+    	return false
+  	}
+	
+	sqlStatement4:= `UPDATE projects SET $1 = array_remove($1, $2) WHERE id = $3;`
+	err = db.QueryRow(sqlStatement4,oldColumn,taskID,parentID)
+	if(err != nil) {
+    	return false
+  	}
+	
+	sqlStatement5:=`UPDATE projects SET $1 = array_cat($1, $2) WHERE id = $3;`
+	err = db.QueryRow(sqlStatement5,newColumn,taskID,parentID)
   	if(err != nil) {
     	return false
   	}
